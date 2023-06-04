4491eee2b0ae7a4d2bda6b04039b536da792f966
Paul Dumitru <99658791+Paul19P@users.noreply.github.com>
2023-06-04 22:36:02 +0300

    Add files via upload

---
+++ src/main/java/exceptions/CustomException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
