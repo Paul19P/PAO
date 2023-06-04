83c29c7960b5841d99b9148d91a652dd05819c65
Paul Dumitru <99658791+Paul19P@users.noreply.github.com>
2023-06-04 22:34:49 +0300

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-import java.util.List;
-public class Main {
-    static List<String> availableCommands = Arrays.asList("help", "end");
-    static List<String> commandsDescriptions = Arrays.asList();
-
-}
